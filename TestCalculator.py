3a515520dbe2830ac56b283ed489547e74a9aafc
Алексей Еременко <138244975+AlexDanish@users.noreply.github.com>
2023-08-08 22:36:15 +0600

    Add files via upload

---
+++ TestCalculator.py
@@ -0,0 +1,18 @@
+from os import name
+import unittest
+from Calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+    def test_add(self):
+        self.assertEqual(self.calculator.add(3,6), 9)
+    def test_subtruct(self):
+        self.assertEqual(self.calculator.subtruct(6,3), 2)
+    def test_muliply(self):
+        self.assertEqual(self.calculator.muliply(3,6), 18)
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(6,3), 2)
+if name == "main":
+    unittest.main()
+                    
